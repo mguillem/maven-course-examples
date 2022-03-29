68d9b3717a4b3724c97bf2a7fbb36926ed6e0316
Marc Guillemot <mguillemot@yahoo.fr>
2022-03-29 10:50:35 +0200

    changed package names, upgraded some plugins

---
+++ exercises/mvn.05.exercise-multi/simple-weather/src/test/java/course/maven/weather/VisualCrossingRetrieverTest.java
@@ -0,0 +1,26 @@
+package course.maven.weather;
+
+import static org.junit.Assert.assertThat;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import course.maven.weather.VisualCrossingRetriever;
+
+public class VisualCrossingRetrieverTest {
+
+	@Test
+	public void simpleTest() throws Exception {
+		VisualCrossingRetriever retriever = new VisualCrossingRetriever();
+		final String content;
+		try (InputStream is = retriever.retrieve("köln")) {
+			content = IOUtils.toString(is);	
+		}
+
+		assertThat(content, CoreMatchers.containsString("Köln, Nordrhein-Westfalen, Deutschland"));
+	}
+
+}
---
+++ exercises/mvn.05.exercise-multi/simple-weather/src/test/java/course/maven/weather/WeatherFormatterTest.java
@@ -0,0 +1,24 @@
+package course.maven.weather;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import course.maven.weather.VisualCrossingParser;
+import course.maven.weather.Weather;
+import course.maven.weather.WeatherFormatter;
+
+public class WeatherFormatterTest {
+
+	@Test
+	public void testFormat() throws Exception {
+		InputStream nyData = getClass().getClassLoader().getResourceAsStream("weather.json");
+		Weather weather = new VisualCrossingParser().parse(nyData);
+		String formattedResult = new WeatherFormatter().format(weather);
+		InputStream expected = getClass().getClassLoader().getResourceAsStream("format-expected.dat");
+		assertEquals(IOUtils.toString(expected), formattedResult);
+	}
+}
